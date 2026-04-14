c9f77f2ec46a30dbabf20a5bbe40ea657e1820a0
Maggie Zhang <52008158+maggiezhangdong@users.noreply.github.com>
2026-04-13 23:29:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("[https://melaniessmoothies-kobwkknayvzp4dvvejenng.streamlit.app/](https://melaniessmoothies-kobwkknayvzp4dvvejenng.streamlit.app/)")  
 st.text(smoothiefroot_response)
